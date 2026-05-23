066194e539d76ee1c90370e84a595e605c226955
rahulchaudhary4490-spec <rahulchaudhary4490@gmail.com>
2026-05-23 22:01:23 +0530

    fix stale connection issue

--- backend/server.py
+++ backend/server.py
@@ -11,10 +11,11 @@ import uom_dao
 app = Flask(__name__)
 CORS(app)
 
-connection = get_sql_connection()
+
 
 @app.route('/getUOM', methods=['GET'])
 def get_uom():
+    connection = get_sql_connection()
     response = uom_dao.get_uoms(connection)
     response = jsonify(response)
     response.headers.add('Access-Control-Allow-Origin', '*')
@@ -22,6 +23,7 @@ def get_uom():
 
 @app.route('/getProducts', methods=['GET'])
 def get_products():
+    connection = get_sql_connection()
     response = products_dao.get_all_products(connection)
     response = jsonify(response)
     response.headers.add('Access-Control-Allow-Origin', '*')
@@ -29,6 +31,7 @@ def get_products():
 
 @app.route('/insertProduct', methods=['POST'])
 def insert_product():
+    connection = get_sql_connection()
     request_payload = json.loads(request.form['data'])
     product_id = products_dao.insert_new_product(connection, request_payload)
     response = jsonify({
@@ -39,6 +42,7 @@ def insert_product():
 
 @app.route('/getAllOrders', methods=['GET'])
 def get_all_orders():
+    connection = get_sql_connection()
     response = orders_dao.get_all_orders(connection)
     response = jsonify(response)
     response.headers.add('Access-Control-Allow-Origin', '*')
@@ -46,6 +50,7 @@ def get_all_orders():
 
 @app.route('/insertOrder', methods=['POST'])
 def insert_order():
+    connection = get_sql_connection()
     request_payload = json.loads(request.form['data'])
     order_id = orders_dao.insert_order(connection, request_payload)
     response = jsonify({
@@ -56,6 +61,7 @@ def insert_order():
 
 @app.route('/deleteProduct', methods=['POST'])
 def delete_product():
+    connection = get_sql_connection()
     return_id = products_dao.delete_product(connection, request.form['product_id'])
     response = jsonify({
         'product_id': return_id
